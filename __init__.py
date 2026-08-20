a72b5e836d417a9bb093eb7e34b0e605276b1cb1
Hetaneko <54069666+Hetaneko@users.noreply.github.com>
2026-08-20 14:58:36 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+try:
+    from typing import override
+except ImportError:
+    from typing_extensions import override
+
+from comfy_api.latest import ComfyExtension, io
+
+from .cosmos_temporal_reference import ApplyCosmosReferenceLatent
+
+
+class CosmosReferenceExtension(ComfyExtension):
+    @override
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [ApplyCosmosReferenceLatent]
+
+
+async def comfy_entrypoint():
+    return CosmosReferenceExtension()
